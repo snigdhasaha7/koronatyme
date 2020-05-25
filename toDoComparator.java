df4e4e3d40113f12802daa78b8efb958eca86d69
brownsnig <63370236+brownsnig@users.noreply.github.com>
2020-05-25 13:19:24 -0700

    Update toDoComparator.java

--- toDoComparator.java
+++ toDoComparator.java
@@ -2,6 +2,10 @@ import java.util.Comparator;
 
 public class toDoComparator implements Comparator<toDoEntry>{
 	
+	public toDoComparator()
+	{
+	}
+	
 	public int compare(toDoEntry toDoEntry1, toDoEntry toDoEntry2)
 	{
 		if(toDoEntry1.getUrgency() > toDoEntry2.getUrgency())
